9219a216df15e48e304be6fb1d844d1a1a161de4
Makanjuola Jomiloju <jomilojumakanjuola@gmail.com>
2025-07-21 13:07:14 +0100

    Add files via upload

---
+++ code/Jomzy/script.js
@@ -0,0 +1,11 @@
+// let a = "123";
+//console.log(a);
+///console.log(typeof a);
+//let b = Number(a);//
+//console.log(b);
+//console.log(typeof b);//
+
+
+let userName = "Jomzy";
+let entered_userName = "Jomzyi";
+console.log(userName == entered_userName);
